2d89d3f31374508661357827b86fcc88cd649fbd
rechielagria <rechielagria@gmail.com>
2020-06-18 05:27:57 +0800

    get add pages

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('admin pages');
+router.get('/', function(req, res, next) {
+	res.send('admin pages');
+});
+
+//ADD PAGES
+router.get('/add_pages', function(req, res, nexy) {
+	var title = '';
+	var slug = '';
+	var content = '';
+	res.render('admin/add_pages', { title: title, slug: slug, content: content });
 });
 
 module.exports = router;
