7e311b0da38886bc8caeacba3f46ec0ba1703166
foxching <rechielagria@gmail.com>
2020-06-18 18:41:40 +0800

    Post Add Page 2

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const { check, validationResult } = require('express-validator');
+var Page = require('../models/page')
 
 // get pages
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 	res.send('admin pages');
 });
 
 //get add page
-router.get('/add_pages', function(req, res, nexy) {
+router.get('/add_pages', function (req, res, nexy) {
 	var title = '';
 	var slug = '';
 	var content = '';
@@ -21,7 +22,7 @@ router.post(
 		check('title', 'Invalid title').not().isEmpty().withMessage('title must not be empty'),
 		check('content', 'Invalid content').not().isEmpty().withMessage('content must not be empty')
 	],
-	function(req, res, next) {
+	function (req, res, next) {
 		var title = req.body.title;
 		var slug = req.body.slug.replace(/\s+/g, '=').toLowerCase();
 		if (slug == '') {
@@ -34,7 +35,27 @@ router.post(
 			//return res.status(422).json({ errors: errors.array() });
 			res.render('admin/add_page', { title: title, slug: slug, content: content, errors: errors.array() });
 		} else {
-			console.log('ok');
+			Page.findOne({ slug: slug }, function (err, page) {
+				if (page) {
+					req.flash('danger', 'Page slug already exists')
+					res.render('admin/add_page', { title: title, slug: slug, content: content, });
+				} else {
+					var page = new Page({
+						title: title,
+						content: content,
+						slug: slug,
+						sorting: 0
+					})
+
+					page.save(function (err) {
+						if (err) {
+							console.log(err)
+						}
+						req.flash('success', 'Page added successfully')
+						res.redirect('/admin/pages')
+					})
+				}
+			})
 		}
 	}
 );
