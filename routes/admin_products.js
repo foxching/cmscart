42f5392f22e48a58681e7a21857a92dc5c07e41a
rechielagria <rechielagria@gmail.com>
2020-06-22 05:48:25 +0800

    Products index

--- routes/admin_products.js
+++ routes/admin_products.js
@@ -4,10 +4,22 @@ var mkdirp = require('mkdirp');
 var fs = require('fs-extra');
 var resizeImg = require('resize-img');
 var Product = require('../models/product');
+var Category = require('../models/category');
 
 // get products
 router.get('/', function(req, res, next) {
-	res.send('Admin Products');
+	var count;
+
+	Product.count(function(err, c) {
+		count = c;
+	});
+
+	Product.find(function(err, products) {
+		res.render('admin/products', {
+			products: products,
+			count: count
+		});
+	});
 });
 
 module.exports = router;
