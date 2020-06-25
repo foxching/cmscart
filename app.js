b8b041cf4acd65aa52e437c2a7b49b899e6f52a7
foxching <rechielagria@gmail.com>
2020-06-25 23:38:47 +0800

    Front menu display

--- app.js
+++ app.js
@@ -11,30 +11,44 @@ if (process.env.NODE_ENV !== 'production') {
 	require('dotenv/config');
 }
 
-var pages = require('./routes/pages');
-var admin_pages = require('./routes/admin_pages');
-var admin_categories = require('./routes/admin_categories');
-var admin_products = require('./routes/admin_products');
 
+//init app
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //express fileupload middleware
 app.use(fileUpload());
 
+// Body Parser middleware
+// parse application/x-www-form-urlencoded
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 //global variables
 app.locals.errors = null;
 
-//express-session
+//Page Model
+var Page = require('./models/page')
+
+//pages variables
+Page.find({}).sort({ sorting: 1 }).exec(function (err, pages) {
+	if (err) {
+		console.log(err)
+	} else {
+		app.locals.pages = pages
+	}
+});
+
+//express-session Middleware
 app.use(
 	session({
 		secret: 'keyboard cat',
@@ -46,28 +60,37 @@ app.use(
 
 //express-messages
 app.use(require('connect-flash')());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	res.locals.messages = require('express-messages')(req, res);
 	next();
 });
+
+//set routes
+var pages = require('./routes/pages');
+var admin_pages = require('./routes/admin_pages');
+var admin_categories = require('./routes/admin_categories');
+var admin_products = require('./routes/admin_products');
 
 app.use('/admin/pages', admin_pages);
 app.use('/admin/categories', admin_categories);
 app.use('/admin/products', admin_products);
 app.use('/', pages);
 
+//mongodb connection
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
 	console.log('Connected to DB');
 });
 mongoose.set('useCreateIndex', true);
 
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
