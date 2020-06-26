0cd21d653623441e520521d427e702ba262e870c
foxching <rechielagria@gmail.com>
2020-06-27 07:31:11 +0800

    Display all products

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.locals.errors = null;
 var Page = require('./models/page');
 
 //pages variables
-Page.find({}).sort({ sorting: 1 }).exec(function(err, pages) {
+Page.find({}).sort({ sorting: 1 }).exec(function (err, pages) {
 	if (err) {
 		console.log(err);
 	} else {
@@ -49,7 +49,7 @@ Page.find({}).sort({ sorting: 1 }).exec(function(err, pages) {
 //Page Model
 var Category = require('./models/category');
 
-Category.find(function(err, categories) {
+Category.find(function (err, categories) {
 	if (err) {
 		console.log(err);
 	} else {
@@ -69,13 +69,14 @@ app.use(
 
 //express-messages
 app.use(require('connect-flash')());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	res.locals.messages = require('express-messages')(req, res);
 	next();
 });
 
 //set routes
 var pages = require('./routes/pages');
+var products = require('./routes/products');
 var admin_pages = require('./routes/admin_pages');
 var admin_categories = require('./routes/admin_categories');
 var admin_products = require('./routes/admin_products');
@@ -83,6 +84,7 @@ var admin_products = require('./routes/admin_products');
 app.use('/admin/pages', admin_pages);
 app.use('/admin/categories', admin_categories);
 app.use('/admin/products', admin_products);
+app.use('/products', products);
 app.use('/', pages);
 
 //mongodb connection
@@ -92,12 +94,12 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedT
 mongoose.set('useCreateIndex', true);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ routes/products.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var Product = require('../models/product')
+
+/* GET all products. */
+router.get('/', function (req, res, next) {
+  Product.find(function (err, products) {
+    if (err) console.log(err)
+    res.render('all_products', { title: 'All Products', products: products })
+  })
+});
+
+
+module.exports = router;
