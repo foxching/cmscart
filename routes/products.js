f7a8e1451c7907487d5c30157e50f4d15aee3bed
rechielagria <rechielagria@gmail.com>
2020-06-27 23:35:17 +0800

    Display products by category

--- routes/products.js
+++ routes/products.js
@@ -1,26 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var Product = require('../models/product')
+var Product = require('../models/product');
+var Category = require('../models/category');
 
 /* 
 * GET all products. 
 */
-router.get('/', function (req, res, next) {
-  Product.find(function (err, products) {
-    if (err) console.log(err)
-    res.render('all_products', { title: 'All Products', products: products })
-  })
+router.get('/', function(req, res, next) {
+	Product.find(function(err, products) {
+		if (err) console.log(err);
+		res.render('all_products', { title: 'All Products', products: products });
+	});
 });
 
 /* 
 * GET products by category. 
 */
-router.get('/:slug', function (req, res, next) {
-  Product.find(function (err, products) {
-    if (err) console.log(err)
-    res.render('all_products', { title: 'All Products', products: products })
-  })
+router.get('/:category', function(req, res, next) {
+	var categorySlug = req.params.category;
+	Category.find({ slug: categorySlug }, function(err, category) {
+		if (err) return console.log(err);
+		Product.find({ category: categorySlug }, function(err, products) {
+			if (err) console.log(err);
+			res.render('cat_products', { title: category.title, products: products });
+		});
+	});
 });
-
 
 module.exports = router;
