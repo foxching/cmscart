682a66297706c3b62215d52b1e5fd19d45ad2eb3
foxching <rechielagria@gmail.com>
2020-06-28 07:52:34 +0800

    Add to Cart

--- routes/cart.js
+++ routes/cart.js
@@ -4,10 +4,47 @@ var router = express.Router();
 var Product = require('../models/product');
 
 /* 
-* . 
+* GET
 */
 router.get('/', function (req, res, next) {
 	res.send('Cart')
+});
+
+router.get('/add/:product', function (req, res, next) {
+	var slug = req.params.product
+	Product.findOne({ slug: slug }, function (err, product) {
+		if (err) return console.log(err)
+		if (typeof req.session.cart == 'undefined') {
+			req.session.cart = []
+			req.session.cart.push({
+				title: slug,
+				qty: 1,
+				price: parseFloat(product.price).toFixed(2),
+				image: 'public/product_images/' + product.id + '/' + product.image
+			})
+		} else {
+			var cart = req.session.cart
+			var newItem = true
+			for (var i = 0; i < cart.length; i++) {
+				if (cart[i].title == slug) {
+					cart[i].qty++
+					newItem = false;
+					break;
+				}
+			}
+			if (newItem) {
+				cart.push({
+					title: slug,
+					qty: 1,
+					price: parseFloat(product.price).toFixed(2),
+					image: 'public/product_images/' + product.id + '/' + product.image
+				})
+			}
+		}
+		//console.log(req.session.cart)
+		req.flash('success', 'Product added to Cart')
+		res.redirect('back')
+	})
 });
 
 
