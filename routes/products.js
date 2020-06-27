1dcbe41114cfdf6f2428ac1f94a9e692511b6e1e
foxching <rechielagria@gmail.com>
2020-06-27 08:16:26 +0800

    Add products to database , fix some issue

--- routes/products.js
+++ routes/products.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 var Product = require('../models/product')
 
-/* GET all products. */
+/* 
+* GET all products. 
+*/
 router.get('/', function (req, res, next) {
+  Product.find(function (err, products) {
+    if (err) console.log(err)
+    res.render('all_products', { title: 'All Products', products: products })
+  })
+});
+
+/* 
+* GET products by category. 
+*/
+router.get('/:slug', function (req, res, next) {
   Product.find(function (err, products) {
     if (err) console.log(err)
     res.render('all_products', { title: 'All Products', products: products })
